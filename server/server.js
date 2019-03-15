02b134299393d949eefd560ff617a60a875951c8
TrungDT <thanhtrung.it25@gmail.com>
2019-03-15 15:17:44 +0700

    users post route and method auth token for user schema

--- server/server.js
+++ server/server.js
@@ -106,6 +106,26 @@ app.patch("/todos/:id", (req, res) => {
     })
 })
 
+// POST /users
+app.post("/users", (req, res) => {
+  console.log(req.body)
+  var body = _.pick(req.body, ["email", "password"])
+  console.log(body)
+  var user = new User(body)
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken()
+    })
+    .then(token => {
+      res.header("x-auth", token).send(user)
+    })
+    .catch(e => {
+      res.status(400).send(e)
+    })
+})
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Started on port 5000")
 })
