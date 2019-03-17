8063ceb9588f5c98cb0be3ff01e55aa1d5920a80
TrungDT <thanhtrung.it25@gmail.com>
2019-03-17 15:43:11 +0700

    Add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,18 @@ UserSchema.methods.generateAuthToken = function() {
   })
 }
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function(token, cb) {
   var User = this
   var decoded
